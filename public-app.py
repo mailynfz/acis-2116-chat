de2a80aeac815b08df97b563a5c2ee0359ac20c3
mailynfz <96663365+mailynfz@users.noreply.github.com>
2023-12-03 18:41:27 -0500

    Update public-app.py

--- public-app.py
+++ public-app.py
@@ -67,21 +67,21 @@ if st.secrets:
         OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
 st.sidebar.markdown(other_text_line_1)
-
-if st.sidebar.button("Start New Chat"):
-    thread = client.beta.threads.create()
-    st.session_state.THREAD_ID = thread.id
-    st.sidebar.write("Thread ID: ", thread.id)
-    st.session_state.messages = []
-
 st.sidebar.markdown(other_text_line_2)
-st.sidebar.markdown(other_text_line_4)
 
 api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 if api_key:
     client = OpenAI(api_key=api_key)
 else:    
     client = OpenAI(api_key=OPENAI_API_KEY)
+    
+if st.sidebar.button("Start New Chat"):
+    thread = client.beta.threads.create()
+    st.session_state.THREAD_ID = thread.id
+    st.sidebar.write("Thread ID: ", thread.id)
+    st.session_state.messages = []
+
+st.sidebar.markdown(other_text_line_4)
 
 st.sidebar.markdown(other_text_line_3)
 
